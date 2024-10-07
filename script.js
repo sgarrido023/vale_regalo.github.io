efec2218b9d227fe661ce19d96c4dda4b8616920
sgarrido023 <sandragarrido023@gmail.com>
2024-10-07 17:44:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('reveal-button').addEventListener('click', function() {
+    document.getElementById('message').classList.remove('hidden');
+    document.getElementById('balloons').classList.remove('hidden');
+    document.getElementById('images-container').classList.remove('hidden'); // Mostrar imágenes
+});
